aef299976a4adfc7cb545f50a3fdb99cfa819e89
David <8584751@qq.com>
2019-06-06 13:59:58 +0800

    Python learning

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,8 @@
+print("Hello world!")
+print("\n")
+count = 0
+while (count < 5):
+   print ('The count is:', count)
+   count = count + 1
+print("\n")
+print("Good bye!")
---
+++ LiaoXueFeng/01_FirstPythonProgram.py
@@ -0,0 +1,2 @@
+print('hello, world')
+print(100 + 200 + 300)
